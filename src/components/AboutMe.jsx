12f8816b5703f191158a45859dbc2bd0479cfa10
ziza <winfourteen@gmail.com>
2024-10-31 23:59:37 +0800

    add about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,7 +15,7 @@ function AboutMe() {
                         I graduated in September 2024 with a Bachelor of Science in Information Technology, major in <span className='text-blue dark:text-neon font-semibold'>Web Application Development</span>. I also completed a 3-month <span className='text-blue dark:text-neon font-semibold'>Full-stack Web Development</span> course at the KodeGo Bootcamp, where I received awards as a Top 5 Student, for Leadership, and for Best Mini Project 1. Currently, I'm a freelancer, collaborating with clients to create dynamic and efficient web applications.
                     </p>
                 </div>
-                <div className='bg-blue-300 flex items-center justify-end relative w-72 h-96 max-[800px]:mt-5 bg-red-300'>
+                <div className='bg-blue-300 flex items-center justify-end relative w-72 h-96 max-[800px]:mt-5'>
                     <div className='absolute inset-0 z-0 top-2 -right-3 left-5 -bottom-5 border-4 border-neon rounded-md'></div> {/* Background frame */}
                     
                     <figure className='relative z-10'> {/* Foreground image */}
